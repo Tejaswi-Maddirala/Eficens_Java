01888139402c7682cd677fe20d1663b372a3d329
Tejaswi Maddirala <tejaswireddym357@gmail.com>
2023-06-03 11:53:52 -0500

    Added Vehicle Class second time

---
+++ com.examples/FirstClass.java
@@ -0,0 +1,9 @@
+package com.examples;
+import com.examples.oops.Vehicle;
+public class FirstClass {
+    public static void main(String[] args){
+        Vehicle car = new Vehicle();
+        car.start();
+        car.stop();
+    }
+}
